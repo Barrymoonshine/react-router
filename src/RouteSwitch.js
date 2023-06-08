a1269d3d2691f7ce1595604e945ac28be6927cf8
Nick Sheward <nickshwrd@gmail.com>
2023-06-08 16:12:50 +0100

    Added Links

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Profile from './Profile';
 import Dont from './Dont';
@@ -9,16 +9,29 @@ import Now from './Now';
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/dont' element={<Dont />} />
-        <Route path='/stop' element={<Stop />} />
-        <Route path='/me' element={<Me />} />
-        <Route path='/now' element={<Now />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/profile'>Profile</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path='/' element={<App />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/dont' element={<Dont />} />
+          <Route path='/stop' element={<Stop />} />
+          <Route path='/me' element={<Me />} />
+          <Route path='/now' element={<Now />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
