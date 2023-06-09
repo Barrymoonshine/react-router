7763325e4f78308f0ec32486d8012184fcd29ebd
Nick Sheward <nickshwrd@gmail.com>
2023-06-09 09:28:56 +0100

    Added useNavigate and useEffect to redirect users back to home from 404 page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/');
+    }, 1000);
+  }, []);
+
   return (
     <div>
       <h1>Error 404 - page not found</h1>
